c194a2d94332ad91e9c73ccfa687ca26e70a5dce
Cody Stanish <stanishcody@gmail.com>
2023-04-15 12:42:24 -0400

    create action buttons with initial styles and roll logic

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
-const dies = document.querySelector('.dies');
-const amountDisplay = document.querySelector('.amount-display')
+const dies = document.querySelector(".dies");
+const amountDisplay = document.querySelector(".amount-display");
+const resultsDisplay = document.querySelector(".results-display");
+const btnRoll = document.querySelector(".roll");
 
-const diceAmount = {'4': 0, '6': 0, '8': 0, '10': 0, '12': 0, '20': 0, '100': 0};
+const diceAmount = { 4: 0, 6: 0, 8: 0, 10: 0, 12: 0, 20: 0, 100: 0 };
 const diceReady = [];
 
 const amountsList = () => {
-    return Object.entries(diceAmount)
-        .filter(([_, qty]) => qty > 0)
-        .map(([dice,qty]) => `${qty}d${dice}`)
-        .join(' + ');
-        
-}
+  return Object.entries(diceAmount)
+    .filter(([_, qty]) => qty > 0)
+    .map(([dice, qty]) => `${qty}d${dice}`)
+    .join(" + ");
+};
 
-dies.addEventListener('click', (event) => {
-    if(event.target.closest('.btn')){
-        const diceType = event.target.dataset.dice;
-        diceAmount[diceType.toString()]++;
-        diceReady.push(diceType);
+dies.addEventListener("click", (event) => {
+  if (event.target.closest(".btn")) {
+    const diceType = event.target.dataset.dice;
+    diceAmount[diceType]++;
+    diceReady.push(diceType);
 
-        amountDisplay.innerHTML = '';
-        amountDisplay.innerHTML = `${amountsList()}`
-        console.log(diceReady)
-    }
-})
+    amountDisplay.innerHTML = "";
+    amountDisplay.innerHTML = `${amountsList()}`;
+    console.log(diceReady);
+  }
+});
+
+btnRoll.addEventListener("click", () => {
+  const result = diceReady.reduce((sum, cur) => {
+    return (sum += Math.floor(Math.random() * cur) + 1);
+  }, 0);
+
+  resultsDisplay.innerHTML = "";
+  resultsDisplay.innerHTML = `${result}`;
+});
