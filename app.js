74517c7fd1225522d6d6013f877279744677a69f
Cody Stanish <stanishcody@gmail.com>
2023-05-04 15:59:09 -0400

    brought dice buttons to the side of display and add expand/collapse logic

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-const dies = document.querySelector(".dies");
+const dies = document.querySelectorAll(".btn");
+const diceMenuBtn = document.querySelector(".dice-menu-btn");
+const menuIcons = document.querySelectorAll(".dice-menu-btn svg");
 const amountDisplay = document.querySelector(".amount-display");
 const resultsDisplay = document.querySelector(".results-display");
 const btnRoll = document.querySelector(".roll");
+const btnClear = document.querySelector(".clear");
 
 const diceAmount = { 4: 0, 6: 0, 8: 0, 10: 0, 12: 0, 20: 0, 100: 0 };
 const diceReady = [];
@@ -13,19 +16,39 @@ const amountsList = () => {
     .join(" + ");
 };
 
-dies.addEventListener("click", (event) => {
-  if (event.target.closest(".btn")) {
-    const diceType = event.target.dataset.dice;
-    diceAmount[diceType]++;
-    diceReady.push(diceType);
+dies.forEach((dice) =>
+  dice.addEventListener("click", (event) => {
+    if (event.target.closest(".btn")) {
+      const diceType = event.target.closest(".btn").dataset.dice;
+      diceAmount[diceType]++;
+      diceReady.push(diceType);
 
-    amountDisplay.innerHTML = "";
-    amountDisplay.innerHTML = `${amountsList()}`;
-    console.log(diceReady);
+      amountDisplay.innerHTML = "";
+      amountDisplay.innerHTML = `${amountsList()}`;
+    }
+  })
+);
+
+diceMenuBtn.addEventListener("click", (event) => {
+  if (event.target.closest(".dice-menu-btn")) {
+    menuIcons.forEach((icon) => icon.classList.toggle("hidden"));
+
+    if (!menuIcons[0].classList.contains("hidden")) {
+      for (let i = 1; i <= dies.length; i++) {
+        dies[i - 1].style.transform = `translateY(-${125 * i}%)`;
+      }
+    } else {
+      for (let i = 1; i <= dies.length; i++) {
+        dies[i - 1].style.transform = `translateY(0%)`;
+      }
+    }
   }
 });
 
 btnRoll.addEventListener("click", () => {
+  if (diceReady.length === 0) {
+    return;
+  }
   const result = diceReady.reduce((sum, cur) => {
     return (sum += Math.floor(Math.random() * cur) + 1);
   }, 0);
